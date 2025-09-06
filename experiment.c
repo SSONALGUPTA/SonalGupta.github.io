24cfdcfbd07ee7f0ace36ec0b0a364cbe6a5dac5
SSONALGUPTA <sonalg974@gmail.com>
2025-09-06 12:36:46 +0530

    temperatureConvert

--- experiment.c
+++ experiment.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+
+void fahrenheitToCel();
 void main (){
     float var = 99.09;
-    printf("%0.4f\n",var); // output -> 90.0900
+   // printf("%0.4f\n",var); // output -> 99.0900
     int a=47;
     printf(" OCTAL value of %d  : %o\n",a,a); //converting it into octal 
     printf("Hexadecimal value of %d : %X \n",a,a);
     int n;
-    printf("Enter the octal num :");
-    scanf("%o",&n);
-    printf("%d\n",n);
+    // printf("Enter the octal num :");
+    // scanf("%o",&n);
+    // printf("%d\n",n);
     
-    
-
-
     //logical operators
       //AND
     (var>90 && 0)? printf("True\n"):printf("False\n"); // ternary operator
@@ -21,5 +20,16 @@ void main (){
        //AND
     int s=10 & 3 ;// 2 
     s= 17 & 9; // 1 binary multiplication
-    printf("%d",s);
+    //printf("%d",s);
+   fahrenheitToCel();
+    
 }
+void fahrenheitToCel(){
+        int f=0;
+        float c;
+         while(f<140){
+            c= 5*((f-32)/9);
+            printf(" Fahrenheit :%d \t Celsius:%.2f\n",f,c);
+            f+=12;
+         }
+    }
